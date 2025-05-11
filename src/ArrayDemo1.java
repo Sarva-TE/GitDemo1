ae9d99164d5d989389b800963f7a9071f69dbe34
Sarva-TE <sarvalakshmi.devu@gmail.com>
2025-05-11 12:43:18 +0530

    First Commit

---
+++ src/ArrayDemo1.java
@@ -0,0 +1,22 @@
+
+
+public class ArrayDemo1 
+{
+
+	public static void main(String[] args) 
+	{
+		int arr[]=new int[5];
+		//int []arr=new int[5];
+		//int[] arr=new int[5];
+		arr[0]=10;
+		System.out.println(arr[0]);
+		System.out.println(arr[0]+10);
+
+		arr[0]=15;
+		System.out.println(arr[0]);
+
+		int a=arr[0];
+		System.out.println(a);
+	}
+
+}
